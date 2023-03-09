af8e69b1a310a0438570861af2b85bcca26d8dc0
Jeferson Oliveira <jheferson.contato@hotmail.com>
2023-03-09 16:21:07 -0300

    alteração da fetch(JS) para biblioteca Axios

--- src/util/load-posts.js
+++ src/util/load-posts.js
@@ -1,11 +1,14 @@
+import axios from 'axios';
+
 export const loadPosts = async () => {
-  const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-  const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+  const postsResponse = axios.get('https://jsonplaceholder.typicode.com/posts');
+
+  const photosResponse = axios.get('https://jsonplaceholder.typicode.com/photos');
 
   const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-  const postsJson = await posts.json();
-  const photosJson = await photos.json();
+  const postsJson = await posts.data;
+  const photosJson = await photos.data;
 
   const postsAndPhotos = postsJson.map((post, index) => {
     return { ...post, cover: photosJson[index].url };
