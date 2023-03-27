88f9b8cd9f7e2005d974251ca0366f214662434f
Jeferson Oliveira <jheferson.contato@hotmail.com>
2023-03-27 14:19:34 -0300

    efeito máquina de escrever no title

--- src/Components/TextGridInitial/index.jsx
+++ src/Components/TextGridInitial/index.jsx
@@ -1,9 +1,37 @@
 /* eslint-disable prettier/prettier */
+import { useEffect, useState } from 'react';
 import * as Styled from './styles';
+
+
 export const TextGridInitial = () => {
+
+  const textToType = 'BEM VINDO AO PROJETO REACT';
+  const typingSpeed = 70;
+
+  const [text, setText] = useState('');
+  const [charIndex, setCharIndex] = useState(0);
+
+  useEffect(()=>{
+
+    if(charIndex >= textToType.length){
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setText(text + textToType[charIndex]);
+      setCharIndex(charIndex + 1);
+    }, typingSpeed);
+
+
+
+    return () => clearTimeout(timer);
+  }, [text, charIndex]);
+
+
+
   return (
     <Styled.Container>
-      <Styled.MainTitle> BEM VINDO AO PROJETO REACT</Styled.MainTitle>
+      <Styled.MainTitle>{text}</Styled.MainTitle>
       <Styled.ContainerContent>
         <Styled.TextInitial>
           Através de pesquisas realizadas em diversas plataformas de ensino, foi possível verificar que 99% possuem
