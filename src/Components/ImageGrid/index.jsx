30fa12df580abba143f563b15a452ef8cb8727f9
Jeferson Oliveira <jheferson.contato@hotmail.com>
2023-03-27 15:35:42 -0300

    atualização da image-grid-final

--- src/Components/ImageGrid/index.jsx
+++ src/Components/ImageGrid/index.jsx
@@ -3,24 +3,41 @@ import * as Styled from './styles';
 export const ImageGrid = () => {
   return (
     <Styled.Wrapper>
-      <Styled.Container>
-        <img
-          src="http://source.unsplash.com/random/360x360?r=1"
-          alt="random image from unsplash"
-        />
-        <img
-          src="http://source.unsplash.com/random/360x360?r=2"
-          alt="random image from unsplash"
-        />
-        <img
-          src="http://source.unsplash.com/random/360x360?r=3"
-          alt="random image from unsplash"
-        />
-        <img
-          src="http://source.unsplash.com/random/360x360?r=4"
-          alt="random image from unsplash"
-        />
-      </Styled.Container>
+      <Styled.BoxContent>
+        <Styled.Title>Tecnologias utilizadas</Styled.Title>
+        <Styled.Container>
+          <Styled.StyleCircle>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"
+              alt="html image icon"
+            />
+          </Styled.StyleCircle>
+          <Styled.StyleCircle>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"
+              alt="css image icon"
+            />
+          </Styled.StyleCircle>
+          <Styled.StyleCircle>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"
+              alt="javascript image icon"
+            />
+          </Styled.StyleCircle>
+          <Styled.StyleCircle>
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"
+              alt="react image icon"
+            />
+          </Styled.StyleCircle>
+          <Styled.StyleCircle>
+            <img
+              src="https://styled-components.com/logo.png"
+              alt="styled-components image icon"
+            />
+          </Styled.StyleCircle>
+        </Styled.Container>
+      </Styled.BoxContent>
     </Styled.Wrapper>
   );
 };
--- src/Components/ImageGrid/styles.js
+++ src/Components/ImageGrid/styles.js
@@ -1,36 +1,61 @@
+/* eslint-disable no-unused-vars */
 import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.section`
+  ${({ theme }) => css`
   background: white;
+  display: flex;
+  width: 100%;
+
+  @media ${theme.media.lteMedium} {
+    display: flex;
+    text-align: center;
+    max-width: 100%;
+    background: #080c26;
+  }
+  `}
+`;
+
+export const BoxContent = styled.div`
+${({ theme }) => css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 300px 0px 0px;
+  border-left: 3px solid #a07918;
+  background: #080c26;
+  width: 100%;
+  padding: ${theme.spacings.xlarge};
+
+
+  @media ${theme.media.lteMedium} {
+    display: flex;
+    max-width: 100%;
+    background: #080c26;
+    border: none;
+
+    img:hover {
+      transform: scale(1.1);
+    }
+  }
+  `}
+`;
+
+export const Title = styled.h1`
+${({ theme }) => css`
+  font-size: ${theme.fonts.sizes.medium};
+  color: white;
+  padding: 30px 0px;
+  text-transform: uppercase;
+  `}
 `;
 
 export const Container = styled.div`
   ${({ theme }) => css`
     display: flex;
     position: relative;
-    grid-template-columns: repeat(4, 1fr);
     gap: ${theme.spacings.large};
-    justify-content: center;
-    width: 100%;
-    padding: ${theme.spacings.xlarge};
-    border-radius: 300px 0px 0px;
-    border-left: 3px solid #a07918;
-    background: #080c26;
-    border-top: 3px solid #a07918;
-
-    img {
-      max-width: 200px;
-      max-height: auto;
-      transition: all 200ms ease-in-out;
-      overflow: hidden;
-      width: 100%;
-      border-radius: 500px 500px;
-      box-shadow: 0 0px 0px 2px #a07918;
-    }
-
-    img:hover {
-      transform: scale(1.1);
-    }
 
     @media ${theme.media.lteMedium} {
       display: grid;
@@ -39,18 +64,29 @@ export const Container = styled.div`
       align-items: center;
       max-width: 100%;
       justify-items: center;
-
-      > img {
-        max-width: auto;
-        max-height: auto;
-        transition: all 200ms ease-in-out;
-        overflow: hidden;
-        width: 100%;
-      }
-
-      img:hover {
-        transform: scale(1.1);
-      }
     }
+  `}
+`;
+
+export const StyleCircle = styled.div`
+  ${({ theme }) => css`
+    width: 120px;
+    height: 100%;
+    transition: all 200ms ease-in-out;
+    overflow: hidden;
+    border-radius: 500px;
+    box-shadow: 0 0px 0px 2px #a07918;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    &:hover {
+      transform: scale(1.1);
+    }
+
+
   `}
 `;
